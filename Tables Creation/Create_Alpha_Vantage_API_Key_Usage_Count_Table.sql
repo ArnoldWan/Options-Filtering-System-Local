1a1e33e444d52a7070f80c6cdfcee2bc56978390
ArnoldWan <tszhangwan@gmail.com>
2024-06-27 09:32:10 +0800

    Update Create_Alpha_Vantage_API_Key_Usage_Count_Table.sql

--- Tables Creation/Create_Alpha_Vantage_API_Key_Usage_Count_Table.sql
+++ Tables Creation/Create_Alpha_Vantage_API_Key_Usage_Count_Table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Alpha_Vantage_API_Key_Usage_Count (
     Usage_ID INT PRIMARY KEY IDENTITY(1,1),
-    API_Key_ID INT,
-    API_KEY VARCHAR(255),
-    Used_On_US_Date DATE,
+    API_Key_ID INT NOT NULL,
+    API_KEY VARCHAR(255) NOT NULL,  
+    Used_On_US_Date DATE NOT NULL,
     Count INT DEFAULT 1,
     Created_On DATETIME DEFAULT GETDATE(),
     Updated_On DATETIME DEFAULT GETDATE(),
